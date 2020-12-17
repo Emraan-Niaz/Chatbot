99f87065a00b0d32e4005e1805576ff5ff6d5951
Emraan Niaz <emraan.niaz@hotmail.com>
2020-12-17 18:37:56 +0100

    Corrections

--- v1/chatbot.py
+++ v1/chatbot.py
@@ -5,27 +5,34 @@
 # (c) 2020 by me, Lizenz GPLv3
 
 import  random
-zufallsantworten = ["Oh wirklich..", "interessant", "Das kann man so sehn", "Ich verstehe..."]
-#Stopwörter
-reaktionen = {"hallo": "aber hallo",
-                "geht": "Was verstehst du darunter",
-                "schmeckt": "Ich habe keinen Geschmacksinn"}
 
+# zufällige Antworten
+zufallsantworten = ["Oh wirklich..", "interessant", "Das kann man so sehen", "Ich verstehe..."]
+
+# Stopwörter
+reaktionen = {"hallo": "Hali hallo",
+                "Wie geht es dir?": "Mir geht es auch gut",
+                "schmeckt": "Ich habe keinen Geschmacksinn",
+                 "warum?" : "Weil ich machine bin :)",
+                 "nein": "Gut", "gut":"Danke", "danke" : "Bitte"}
+
+# Ausgabe Head..
 print ("Wilkommen beim ChatBot  (v1)")
 print("Worüber wollen Sie sprechen")
 print("Zum Beenden geben Sie bye ein...")
 print("")
 
+# main
 nutzereingabe = ""
 while nutzereingabe != "bye":
     nutzereingabe = ""
     while nutzereingabe == "":
-     nutzereingabe = input("Ihre Fragen oder Antwort:")
+         nutzereingabe = input("Ihre Fragen oder Antwort: ")
     nutzereingabe = nutzereingabe.lower()
-    nutzereingabe = nutzereingabe.split()
+    nutzerwoerter = nutzereingabe.split()
 
     intelligentAntworten = False
-    for einzelworter in nutzereingabe:
+    for einzelworter in nutzerwoerter:
         if einzelworter in reaktionen:
             print(reaktionen[einzelworter])
             intelligentAntworten = True
