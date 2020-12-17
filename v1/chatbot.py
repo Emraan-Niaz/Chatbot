bc2b87bbf6015c5c95525007ab872292f4a43fb2
Emraan Niaz <emraan.niaz@hotmail.com>
2020-12-17 17:34:34 +0100

    Aenderung

--- v1/chatbot.py
+++ v1/chatbot.py
@@ -4,13 +4,13 @@
 # Ein einfacher ChatBot
 # (c) 2020 by me, Lizenz GPLv3
 
-import random
-#zufallsantworten = ["Oh wirklich..", "interessant", "Das kann man so sehn", "Ich verstehe..."]
+import  random
+zufallsantworten = ["Oh wirklich..", "interessant", "Das kann man so sehn", "Ich verstehe..."]
 #Stopwörter
-reaktionen = {"hallo", "aber hallo",
-                "geht", "Was verstehst du darunter"
-                "schmeckt", "Ich habe keinen Geschmacksinn"
-             }
+reaktionen = {"hallo": "aber hallo",
+                "geht": "Was verstehst du darunter",
+                "schmeckt": "Ich habe keinen Geschmacksinn"}
+
 print ("Wilkommen beim ChatBot  (v1)")
 print("Worüber wollen Sie sprechen")
 print("Zum Beenden geben Sie bye ein...")
@@ -19,17 +19,16 @@ print("")
 nutzereingabe = ""
 while nutzereingabe != "bye":
     nutzereingabe = ""
-    while nutzereingabe =="":
-    nutzereingabe = input("Ihre Fragen oder Antwort:")
+    while nutzereingabe == "":
+     nutzereingabe = input("Ihre Fragen oder Antwort:")
     nutzereingabe = nutzereingabe.lower()
     nutzereingabe = nutzereingabe.split()
 
     intelligentAntworten = False
     for einzelworter in nutzereingabe:
         if einzelworter in reaktionen:
-            intelligentAntworten = True
-        if not intelligentAntworten:
             print(reaktionen[einzelworter])
-
-    #print(random.choice(zufallsantworten))
+            intelligentAntworten = True
+    if not intelligentAntworten:         
+         print(random.choice(zufallsantworten))
 print("einen schönen Tag.")
